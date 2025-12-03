6e3da22ec55c190909f75ab139eeed644f59ec88
251654 Urbaniak Mateusz <251654@edu.p.lodz.pl>
2025-12-03 16:50:33 +0100

    spring boot init

---
+++ src/main/java/com/szebi/controloptimilizationmodule/ControlOptimilizationModuleApplication.java
@@ -0,0 +1,13 @@
+package com.szebi.controloptimilizationmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlOptimilizationModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlOptimilizationModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/szebi/controloptimilizationmodule/ControlOptimilizationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szebi.controloptimilizationmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlOptimilizationModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
